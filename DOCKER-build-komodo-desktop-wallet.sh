7fc4b1a67bca5dcab36aa5221f2d9fda79c77597
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-07-02 06:36:07 -0700

    Update DOCKER-build-komodo-desktop-wallet.sh

--- DOCKER-build-komodo-desktop-wallet.sh
+++ DOCKER-build-komodo-desktop-wallet.sh
@@ -1,4 +1,4 @@
-#docker run -it --net host -d -v /root/komodo-wallet-desktop:/opt/komodo-desktop-wallet debian:10
+#docker run -it --net host -d -v /root/komodo-wallet-desktop:/opt/komodo-desktop-wallet debian:12
 #docker exec -it <docker_hash_here> bash
 
 
