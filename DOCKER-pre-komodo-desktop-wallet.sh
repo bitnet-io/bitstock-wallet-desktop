e3798fdb9a49729e0244c96b1ece1221efaef79c
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-07-07 18:44:56 -0700

    Update DOCKER-pre-komodo-desktop-wallet.sh

--- DOCKER-pre-komodo-desktop-wallet.sh
+++ DOCKER-pre-komodo-desktop-wallet.sh
@@ -1,5 +1,9 @@
 docker run -it --net host -d -v /root/komodo-wallet-desktop:/opt/komodo-desktop-wallet debian:10
 docker exec -it <docker_hash_here> bash
 
+wget https://raw.githubusercontent.com/bitnet-io/bitstock-wallet-desktop/main/DOCKER-post-komodo-desktop-wallet.sh
+chmod +x DOCKER-post-komodo-desktop-wallet.sh
 
+./DOCKER-post-komodo-desktop-wallet.sh
+echo 'then run'
 cmake --build . --config Release --target komodo-wallet
