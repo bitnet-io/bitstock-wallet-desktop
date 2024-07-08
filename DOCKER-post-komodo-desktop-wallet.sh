01cc7bb523e3f61c71256b165602fc66a83fce5a
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-07-08 00:32:10 -0700

    Update DOCKER-post-komodo-desktop-wallet.sh

--- DOCKER-post-komodo-desktop-wallet.sh
+++ DOCKER-post-komodo-desktop-wallet.sh
@@ -129,11 +129,11 @@ echo ''
 echo 'building for an appimage must be done on the host or with PID1 in docker running'
 #cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release ../ 
 
-cmake  -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -GNinja ../
+DESTDIR=AppDir cmake  -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -GNinja ../
 
-cmake --build . --config Release --target komodo-wallet
+DESTDIR=AppDir cmake --build . --config Release --target komodo-wallet
 
-ninja -j24 install DESTDIR=AppDir
+DESTDIR=AppDir ninja -j24 install 
 
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20240109-1/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
