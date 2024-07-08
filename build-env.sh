74500be5698fc5e97f7ee98162dc4dd699631967
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-07-07 19:28:53 -0700

    Update build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -1,6 +1,4 @@
-wget https://raw.githubusercontent.com/bitnet-io/bitstock-wallet-desktop/main/wget.sh
-chmod +x wget.sh
-./wget.sh
+
 
 wget https://raw.githubusercontent.com/bitnet-io/bitstock-wallet-desktop/main/DOCKER-post-komodo-desktop-wallet.sh
 chmod +x DOCKER-post-komodo-desktop-wallet.sh
