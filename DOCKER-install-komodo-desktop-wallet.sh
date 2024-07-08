d2de7f92ffd8214adbf61071e6b42c57921f48d5
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-07-07 19:37:40 -0700

    Update DOCKER-install-komodo-desktop-wallet.sh

--- DOCKER-install-komodo-desktop-wallet.sh
+++ DOCKER-install-komodo-desktop-wallet.sh
@@ -1,3 +1,4 @@
+mkdir  /root/komodo-wallet-desktop
 docker run -it --net host -d -v /root/komodo-wallet-desktop:/opt/komodo-desktop-wallet debian:10
 sleep 1s
 echo 'docker exec -it <docker_hash_here> bash'
