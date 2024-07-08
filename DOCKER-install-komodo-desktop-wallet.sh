1365546f6c09f43ae68bbfe19a2d4a95fcf0d909
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-07-07 19:24:36 -0700

    Update DOCKER-install-komodo-desktop-wallet.sh

--- DOCKER-install-komodo-desktop-wallet.sh
+++ DOCKER-install-komodo-desktop-wallet.sh
@@ -6,14 +6,7 @@ sleep 2s
 
 
 
-wget https://raw.githubusercontent.com/bitnet-io/bitstock-wallet-desktop/main/wget.sh
-chmod +x wget.sh
-./wget.sh
 
-wget https://raw.githubusercontent.com/bitnet-io/bitstock-wallet-desktop/main/DOCKER-post-komodo-desktop-wallet.sh
-chmod +x DOCKER-post-komodo-desktop-wallet.sh
-
-./DOCKER-post-komodo-desktop-wallet.sh
 
 echo 'then run'
 cmake --build . --config Release --target komodo-wallet
